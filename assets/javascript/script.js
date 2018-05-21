082b00b9ba9f0f050f40ed416e0e37ce9d02501f
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-05-20 21:49:54 -0400

    added api to get gifs, added function to create gif img tag and post to screen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,41 @@
-    var topics = ['dogs', 'cats', 'women', 'gay', 'motorbikes', 'sushi', ''];
+var topics = ['gay', 'women', 'cars', 'motorbie'];
+
+// var myKey = 'oAPKq0ycl46LlXlQ6CpSi5k9jK2cPOqB';
+// var userTag = 'dog';
+// // var userTag = document.querySelector('#myTag').value;
+// var queryURL = `https://api.giphy.com/v1/gifs/trending?api_key=${myKey}&q=${userTag}`;
+//
+//
+//
+$("#gifBTN").on("click", function () {
+  event.preventDefault();
+  var myKey = 'oAPKq0ycl46LlXlQ6CpSi5k9jK2cPOqB';
+  // var userTag = 'dogs';
+  var userTag = document.querySelector('#myTag').value;
+  var queryURL = `https://api.giphy.com/v1/gifs/random?api_key=${myKey}&tag=${userTag}`;
+//
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    console.log(queryURL);
+    var imageUrl = response.data.image_original_url;
+    console.log(imageUrl);
+    myImage(imageUrl, userTag);
 
 
 
+  });
+});
 
+function myImage(image, tag) {
+  var gifImage = $("<img>");
 
+  // Setting the Image src attribute to imageUrl
+  gifImage.attr("src", image);
+  gifImage.attr("alt", `${tag} image`);
 
-    // Example queryURL for Giphy API
-    var apiKey = 'oAPKq0ycl46LlXlQ6CpSi5k9jK2cPOqB';
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=" + apiKey;
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-    });
+  // Prepending the Image to the images div
+  $("#gifDisplay").prepend(gifImage);
+}
